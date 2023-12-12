18db56ad819b17a6a5ee7278a08c46bbb87c2735
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-12-12 16:12:55 +0100

    Add screens and navigation

---
+++ frontend/CashManager/screens/Home.js
@@ -0,0 +1,17 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+export default function Home() {
+
+  const navigation = useNavigation();
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <TouchableOpacity
+      onPress={()=>{navigation.navigate('Connection')}}>
+        <Text>GotoConnection</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
