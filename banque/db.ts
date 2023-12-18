01871a90ef55ef1cbcb1e4a936fe68aaa4929f0d
Yvan Roux <yvis99@gmail.com>
2023-12-18 16:04:54 +0100

    [BD] script d'insertion

---
+++ banque/db.ts
@@ -0,0 +1,45 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mysql = require('mysql')
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'BoisdeCen2&*',
+  database: 'cash_manager_db'
+})
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à la base de données : ', err)
+    connection.end()
+  } else {
+    console.log('Connecté à la base de données!')
+
+    const createDatabaseQuery = 'CREATE DATABASE IF NOT EXISTS cash_manager_db'
+
+    connection.query(createDatabaseQuery, (databaseError, databaseResults) => {
+      if (databaseError) {
+        console.error('Erreur lors de la création de la base de données : ', databaseError)
+      } else {
+        console.log('Base de données "cash_manager_db" créée avec succès :', databaseResults)
+
+        const createTableAccountQuery = `
+          CREATE TABLE IF NOT EXISTS account (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            money DECIMAL(10, 2)
+          )        `
+
+        connection.query(createTableAccountQuery, (tableError, tableResults) => {
+          if (tableError) {
+            console.error('Erreur lors de la création de la table product: ', tableError)
+          } else {
+            console.log('Table "product" créée avec succès :', tableResults)
+          }
+          connection.end()
+        })
+      }
+
+    })
+  }
+})
