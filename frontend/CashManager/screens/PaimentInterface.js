9550b72b5dd535c218bf712a0407485c640d59a8
Julie Nussbaumer <jnussbaumer@libriciel.coop>
2023-12-18 16:46:22 +0100

    Add qrcode scanner lib

--- frontend/CashManager/screens/PaimentInterface.js
+++ frontend/CashManager/screens/PaimentInterface.js
@@ -5,7 +5,9 @@ import {
   View,
 } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Camera } from 'expo-camera';
 
 export default function PaimentInterface() {
 
@@ -29,25 +31,99 @@ export default function PaimentInterface() {
     }
   ]);
 
+
+  // return (
+  //
+  //   <View style={styles.container}>
+  //     <Text style={styles.title}>Paiment</Text>
+  //
+  //     <Text
+  //       style={styles.categoryContainer}>{bill.reduce((price, product) => price = price + product.price, 0)} €</Text>
+  //
+  //
+  //     <TouchableOpacity style={styles.button}
+  //                       onPress={() => {
+  //                         navigation.navigate('BillInterface');
+  //                       }}>
+  //       <Text>Précédent</Text>
+  //     </TouchableOpacity>
+  //   </View>
+  // );
+
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const handleBarCodeScanned = ({
+    type,
+    data
+  }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  const renderCamera = () => {
+    return (
+      <View style={styles.cameraContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={styles.camera}
+        />
+      </View>
+    );
+  };
+
+  if (hasPermission === null) {
+    return <View/>;
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Camera permission not granted</Text>
+      </View>
+    );
+  }
+
   return (
-
     <View style={styles.container}>
-      <Text style={styles.title}>Paiment</Text>
-
-      <Text style={styles.categoryContainer}>{bill.reduce((price,product) =>  price = price + product.price , 0 )} €</Text>
-
-
-      <TouchableOpacity style={styles.button}
-                        onPress={() => {
-                          navigation.navigate('BillInterface');
-                        }}>
-        <Text>Précédent</Text>
+      <Text style={styles.title}>Welcome to the Barcode Scanner App!</Text>
+      <Text style={styles.paragraph}>Scan a barcode to start your job.</Text>
+      {renderCamera()}
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => setScanned(false)}
+        disabled={scanned}
+      >
+        <Text style={styles.buttonText}>Scan QR to Start your job</Text>
       </TouchableOpacity>
     </View>
   );
+
+
 }
 
 const styles = StyleSheet.create({
+    paragraph: {
+      fontSize: 16,
+      marginBottom: 40,
+    },
+    cameraContainer: {
+      width: '80%',
+      aspectRatio: 1,
+      overflow: 'hidden',
+      borderRadius: 10,
+      marginBottom: 40,
+    },
+    camera: {
+      flex: 1,
+    },
     container: {
       flex: 1,
       alignItems: 'center',
