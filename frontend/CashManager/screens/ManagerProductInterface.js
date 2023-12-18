2777b236a4af5f83af9a90091b1be3187fd65959
kevin ly <kev3010@outlook.fr>
2023-12-18 10:00:37 +0100

    home screen + componenent product card

---
+++ frontend/CashManager/screens/ManagerProductInterface.js
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function ManagerProductInterface() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Product Interface Screen</Text>
+    </View>
+  );
+}
